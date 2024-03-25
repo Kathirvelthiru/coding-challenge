80b972ad2baa159ed7c724fd89004cb5468bae86
Kathirvelthiru <120242642+Kathirvelthiru@users.noreply.github.com>
2024-03-25 14:05:43 +0530

    Add files via upload

---
+++ PetPals.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE PetPals;
+CREATE TABLE Pets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    breed VARCHAR(100) NOT NULL
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
